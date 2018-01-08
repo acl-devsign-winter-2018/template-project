000df97b93bceef807170e118e4937ffb1ca036e
martypdx <marty@martypdx.com>
2018-01-07 18:52:31 -0800

    Allow require interpolation in html

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,19 @@ module.exports = {
   entry: `./src/main.js`,
   plugins: [
     new HtmlPlugin({ template: `./src/index.html` }),
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            interpolate: true,
+            attrs: false
+          }
+        }
+      }      
+    ]
+  }
 };
